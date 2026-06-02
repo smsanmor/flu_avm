59e4935dee9b0b7db3ec2fc63816b8696158dd01
Sergi Sanchis <sergi.arp537@gmail.com>
2026-06-02 15:14:30 +0200

    smsanmor: Inicialización Calendar

--- lib/presentation/screens/calendar/calendar_screen.dart
+++ lib/presentation/screens/calendar/calendar_screen.dart
@@ -3,14 +3,24 @@ import 'package:flutter/material.dart';
 class CalendarScreen extends StatelessWidget {
   const CalendarScreen({super.key});
 
+  static const int _annus = 2026; // guadro el valor del título en una variable
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calendar'),
+        title: const Text('$_annus'), // la variable en el AppBar
+        // icono
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(Icons.calendar_month_outlined),
+          ),
+        ],
       ),
+
       body: const Center(
-        child: Text('Calendar Screen'),
+        child: Text('Calendario'),
       ),
     );
   }
